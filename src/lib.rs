a9ffd8e38771e2c6c88b0558a14e1f0e27796a60
Shinji Teramoto <shinji.toys@gmail.com>
2021-06-21 11:03:42 +0900

    add ch06

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 pub mod object;
+
+// use fs::FileSystem;
+use object::blob::Blob;
+use object::commit;
+use object::commit::Commit;
+use object::tree;
+use object::tree::Tree;
+use object::GitObject;
+use std::io;
+use std::io::prelude::*;
+
+// #[derive(Debug)]
+// pub struct Git<F: FileSystem> {
+//   pub file_system: F,
+// }
+//
+// impl<F: FileSystem> Git<F> {}
